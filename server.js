2895b616b93240f7f4dc63db8c6fb7bdc292e4a6
Sachin Suryawanshi <sachinsuryawanshi117@gmail.com>
2025-03-13 01:30:56 +0530

    Initial commit, Basic setup with db connection and .gitignore

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
