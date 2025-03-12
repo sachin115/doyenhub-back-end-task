e9573965cce41baabaa77ac31e56b746ee52fc00
Sachin Suryawanshi <sachinsuryawanshi117@gmail.com>
2025-03-13 01:46:41 +0530

    created register and login api

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,8 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+app.use("/api", authRoutes)
 
 // Connect to MongoDB
 mongoose
